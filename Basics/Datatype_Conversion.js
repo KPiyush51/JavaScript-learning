8a8cbdfbd3d717a271a8fed586108aa7a272dc52
KPiyush51 <piyushkumarcreane2019@gmail.com>
2025-02-15 21:51:37 +0000

    Operations

---
+++ Basics/Datatype_Conversion.js
@@ -0,0 +1,45 @@
+let num = 23
+
+let booleanNum = Boolean(num)
+// console.log(booleanNum);
+
+let string = "33abc"
+
+let stringtoNum = Number(string)
+
+// console.log(typeof stringtoNum);
+
+let num1 = "45abc"
+
+let numtoString = String(num1)
+
+// console.log(numtoString);
+
+//  *************************** operations *********************
+
+// console.log(2*2)
+// console.log(2**4);
+// console.log(27%5);
+// console.log("1"+2+4);
+// console.log(1+2+"5")
+// console.log(1+4+"3"+5);
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log("3">1)
+console.log(undefined>0);
+
+
+
+
+
+
+
