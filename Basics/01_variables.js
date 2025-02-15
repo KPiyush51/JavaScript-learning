ceeb8655fc50e689e07d90e148d4fa7ece4a4579
KPiyush51 <piyushkumarcreane2019@gmail.com>
2025-02-15 21:07:18 +0000

    Variables & Datatypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 82
+let accountEmail = "pk@gmail.com"
+var accountName = "Pk"
+accountCity = "Gaya"
+
+// console.log(accountEmail);
+
+let accountState
+
+// console.table([accountId, accountEmail,accountName,accountCity,accountState])
+
+//  Int , bigint, boolean => true/false
+// string => " " 
+//  null - standalone value
+//  undefined 
+// symbol - to define uniqueness
+
+console.log(typeof null);        /* null typeof is object */
+console.log(typeof undefined)     /* typeof of undefined is undefined */
+console.log(typeof "piyush");
