600cfc91fb8431487f40da528fe02da710488b49
KPiyush51 <piyushkumarcreane2019@gmail.com>
2025-02-16 21:53:08 +0000

    String

---
+++ Basics/String.js
@@ -0,0 +1,19 @@
+const name = 'piyush'
+console.log(name);
+
+const Name = new String('piyush')
+const NewName = Name;
+console.log(NewName.length);
+console.log(NewName.toUpperCase());
+console.log(NewName.slice(-4,5));
+console.log(Name);
+
+
+console.log(NewName.charAt(4));
+console.log(NewName.indexOf('t'));
+
+let mainName = "Piyush"
+let AGE = 24
+let college = "NITT"
+
+console.log(`my name is ${mainName} and age is ${AGE} and college is ${college}`)
