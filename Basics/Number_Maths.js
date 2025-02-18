e620eb42d1430385b7155e654bacc9f8a1e32abb
KPiyush51 <piyushkumarcreane2019@gmail.com>
2025-02-18 21:27:19 +0000

    Number & Maths

---
+++ Basics/Number_Maths.js
@@ -0,0 +1,31 @@
+const Number1 = 400;
+// console.log(Number1.toString());
+// console.log(Number1)
+
+const num = new Number(300)
+
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+// console.log(num.toLocaleString('en-IN'))
+
+const num1 = new Number(123.7957)
+
+// console.log(num1.toPrecision(3));
+
+// *******************************************************************************************
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(456.783));
+// console.log(Math.ceil(4.5));
+
+// Math.random always gives value between 0 and 1
+console.log(Math.random()); 
+console.log((Math.random()*10)+1);
+
+const max =20;
+const min =10;
+
+console.log(Math.floor((Math.random()* (max-min+1)))+min);
+
+
