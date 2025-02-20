113342582bffffde92e12b6933519fb98218ad23
KPiyush51 <piyushkumarcreane2019@gmail.com>
2025-02-20 20:01:47 +0000

    Array & Readme

---
+++ Basics2/Array.js
@@ -0,0 +1,39 @@
+const myArr = [0,1,2,3,4,5,6,7,8]
+
+// console.log(myArr)
+// console.log(myArr.includes(5))
+myArr.push(3)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+myArr.unshift(2)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.shift());
+
+
+
+// console.log(myArr);
+
+// console.log(myArr.join());
+
+// **********************************
+
+let Arr1 = new Array(1,2,3,4,5,8,9,6)
+
+
+
+console.log("A ", Arr1);
+
+console.log(Arr1.slice(2, 5))
+console.log("B ",Arr1);
+
+console.log(Arr1.splice(2,5));
+console.log("C ", Arr1);
+
+
+
+
